439b829fbfd6d898c81e959b9a5fcd0dfbafc61b
Nitano0614 <nitano0614@docomo.ne.jp>
2022-06-30 11:50:18 +0900

    トップ画面（修正）

--- app/src/main/java/com/example/quiz_3/MainActivity.kt
+++ app/src/main/java/com/example/quiz_3/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.quiz_3
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Viewの取得
+        val GenreEnter : Button = findViewById(R.id.GenreEnter)
         val spinner: Spinner = findViewById(R.id.spinner)
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
@@ -31,8 +33,8 @@ class MainActivity : AppCompatActivity() {
             //インテントの作成
             val intent = Intent(this, QuizActivity::class.java)
             //データをセット
-            val editText = findViewById<View>(R.id.editText) as EditText
-            intent.putExtra("sendText", editText.text.toString())
+            //val editText = findViewById<View>(R.id.editText) as EditText
+            //intent.putExtra("sendText", editText.text.toString())
             //遷移先の画面を起動
             startActivity(intent)
         }
