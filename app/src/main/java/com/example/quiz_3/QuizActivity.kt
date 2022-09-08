8eb647b8f58f0c76fd1117d869be7b5ad12bf38c
Tanaka <moon.kiho.premier@gmail.com>
2022-09-08 13:39:05 +0900

    本日作業分

--- app/src/main/java/com/example/quiz_3/QuizActivity.kt
+++ app/src/main/java/com/example/quiz_3/QuizActivity.kt
@@ -1,6 +1,8 @@
 package com.example.quiz_3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,5 +15,12 @@ class QuizActivity : AppCompatActivity() {
 
         val genre = intent.getStringExtra("VALUE")
         //genreText.text = "${genre}"
+        val modoruButton : Button = findViewById(com.example.quiz_3.R.id.modoruButton)
+        modoruButton.setOnClickListener {
+            //インテントの作成
+            val intent = Intent(this, MainActivity::class.java)
+            //遷移先の画面を起動
+            startActivity(intent)
+        }
     }
 }
